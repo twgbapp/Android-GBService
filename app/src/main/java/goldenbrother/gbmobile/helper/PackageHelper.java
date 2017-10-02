925605d39f66087d2b55ffdab26de1ff3b3acabc
HaoJun <rin841005@gmail.com>
2017-10-02 15:53:17 +0800

    add Badge Number

--- app/src/main/java/goldenbrother/gbmobile/helper/PackageHelper.java
+++ app/src/main/java/goldenbrother/gbmobile/helper/PackageHelper.java
@@ -5,59 +5,27 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.os.Build;
 
 import java.util.List;
 
+import me.leolin.shortcutbadger.ShortcutBadger;
+
 
 public class PackageHelper {
-    public static String getVersionName(Context context){
+    public static String getVersionName(Context context) {
         PackageManager pm = context.getPackageManager();
-        try{
-            PackageInfo packageInfo = pm.getPackageInfo(context.getPackageName(),0);
+        try {
+            PackageInfo packageInfo = pm.getPackageInfo(context.getPackageName(), 0);
             return packageInfo.versionName;
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
-    }
-    public static int getVersionCode(Context context){
-        PackageManager pm = context.getPackageManager();
-        try {
-            PackageInfo packageInfo = pm.getPackageInfo(context.getPackageName(),0);
-            return packageInfo.versionCode;
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return -1;
     }
 
     public static void setBadge(Context context, int count) {
-        String launcherClassName = getLauncherClassName(context);
-        if (launcherClassName == null) {
-            return;
-        }
-        Intent intent = new Intent("android.intent.action.BADGE_COUNT_UPDATE");
-        intent.putExtra("badge_count", count);
-        intent.putExtra("badge_count_package_name", context.getPackageName());
-        intent.putExtra("badge_count_class_name", launcherClassName);
-        context.sendBroadcast(intent);
-    }
-
-    public static String getLauncherClassName(Context context) {
-
-        PackageManager pm = context.getPackageManager();
-
-        Intent intent = new Intent(Intent.ACTION_MAIN);
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-
-        List<ResolveInfo> resolveInfos = pm.queryIntentActivities(intent, 0);
-        for (ResolveInfo resolveInfo : resolveInfos) {
-            String pkgName = resolveInfo.activityInfo.applicationInfo.packageName;
-            if (pkgName.equalsIgnoreCase(context.getPackageName())) {
-                String className = resolveInfo.activityInfo.name;
-                return className;
-            }
-        }
-        return null;
+        LogHelper.d("Manufacturer : " + Build.MANUFACTURER);
+        ShortcutBadger.applyCount(context, count);
     }
 }
