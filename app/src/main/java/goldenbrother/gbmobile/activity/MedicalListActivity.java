682a7afb1057569862c9e8abb3ac55e5456fc9d8
Unknown <twgbapp@gmail.com>
2017-10-16 18:47:21 +0800

    test2

--- app/src/main/java/goldenbrother/gbmobile/activity/MedicalListActivity.java
+++ app/src/main/java/goldenbrother/gbmobile/activity/MedicalListActivity.java
@@ -46,7 +46,6 @@ public class MedicalListActivity extends CommonActivity implements View.OnClickL
         findViewById(R.id.iv_medical_list_search).setOnClickListener(this);
         findViewById(R.id.iv_medical_list_add).setOnClickListener(this);
         rv = (RecyclerView) findViewById(R.id.rv_medical_list);
-        //test
 
         // init RecyclerView
         list_medical = new ArrayList<>();
